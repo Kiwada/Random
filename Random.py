9d060d6996d393f7620b5ea543cd5a9b11559982
Kiwada <82303943+Kiwada@users.noreply.github.com>
2022-12-19 09:17:20 -0300

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,8 @@
+from random import choice
+n1 = str(input('Primeiro aluno:'))
+n2 = str(input('Segundo Aluno:'))
+n3 = str(input('Terceiro Aluno:'))
+n4 = str(input('Quatro Aluno:'))
+lista = [n1,n2,n3,n4]
+escolhido =choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
